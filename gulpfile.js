2b7f67c61e9815d3cd95ff72f8968d5181e47a59
YAGolubev <yagolubev@inbox.ru>
2016-06-24 01:54:05 +0300

    Добавить sourseMaps + autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,55 @@
 'use strict';
 
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const rename = require('gulp-rename');
+const gulp         = require('gulp');
+const sass         = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const rename       = require('gulp-rename');
+const browserify   = require('browserify');
+
+const vinylSource = require('vinyl-source-stream');
 const browserSync = require('browser-sync').create();
+const plumber     = require('gulp-plumber');
+const notify      = require('gulp-notify');
+const sourceMaps  = require('gulp-sourcemaps');
 
-gulp.task('default', ['css', 'watch']);
+gulp.task('default', ['css', 'js', 'watch']);
 
-gulp.task('css', function(){
+gulp.task('css', function () {
     return gulp.src('./assets/css/app.scss')
-        .pipe(sass())
+        .pipe(plumber({
+            errorHandler: notify.onError('SASS error: <%= error.message %>')
+        }))
+        .pipe(sourceMaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed' //css nano - is better for production
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 1 version']
+        }))
         .pipe(rename('combined.css'))
+        .pipe(sourceMaps.write())
         .pipe(gulp.dest('./build'))
         .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function (){
+gulp.task('js', function () {
+    return browserify('./assets/js/bootstrap.js', {debug: true})
+        .bundle().on('error', function errorHandler(error) {
+            // Array.from()
+            var args = Array.prototype.slice.call(arguments);
+            notify.onError('Browserify error: <%= error.message %>').apply(this, args);
+            this.emit('end'); //stop gulp
+        })
+        .pipe(vinylSource('combined.js'))
+        .pipe(gulp.dest('./build'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function () {
     browserSync.init({
-        proxy: 'localhost:8880',
-        port: 3001,
-        open: false,
+        proxy:  'localhost:8880',
+        port:   3001,
+        open:   false,
         notify: false
     });
     // gulp.watch([
@@ -29,5 +59,10 @@ gulp.task('watch', function (){
         'css/*',
         'css/**',
         'css/**/*.scss'
-     ], {cwd: './assets'}, ['css']);
+    ], {cwd: './assets'}, ['css']);
+    gulp.watch([
+        'js/*',
+        'js/**',
+        'js/**/*.scss'
+    ], {cwd: './assets'}, ['js']);
 });
